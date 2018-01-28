9cdaa5b55aa2b366b65b637765c97c29bb618bda
Eric Kim <bosung90@gmail.com>
2018-01-28 01:48:22 -0800

    Added GameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager Instance;
+	public Dictionary<int, Role> playerRoles = new Dictionary<int, Role>();
+	public Dictionary<int, List<Action>> playerActions = new Dictionary<int, List<Action>>();
+
+	public enum Role
+	{
+		Mobility,
+		Hacker,
+		Combat,
+		Transmitter
+	}
+
+	public struct Action {
+		Role role;
+		int point;
+	}
+
+	void Awake() {
+		if (Instance == null) {
+			Instance = this;
+			DontDestroyOnLoad (this.gameObject);
+		} else {
+			Destroy (this.gameObject);
+		}
+	}
+
+	[ClientRpc]
+	public void InitPlayerRoles(Dictionary<int, Role> playerRoles){
+		this.playerRoles = playerRoles;
+		foreach (int connectionId in playerRoles.Keys) {
+			
+		}
+	}
+}
